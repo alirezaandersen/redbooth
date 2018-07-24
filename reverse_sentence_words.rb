d07a331f95b3000a2572921c8b84369bd9a7f2b3
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-24 15:01:20 -0600

    solution

---
+++ reverse_sentence_words.rb
@@ -0,0 +1,22 @@
+# Write a function that takes a string as an argument and returns a
+# string such that each space-delimited word in the resulting
+# string is the reverse of that in the source string, but the words themselves
+# are in the same order as they are in the source string.
+
+# Example:
+#   Input: "I  like wearing a hat "
+#   Output: "I  ekil gniraew a tah "
+#
+class ReverseSentence
+
+  def self.reverse_sentence_words(string)
+    string. split(/\s+/).map{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
+  end
+
+  def self.reverse_sentence_words(string)
+    reversal = string.split(' ').each do |x| x.reverse!
+    end
+    reversal.join(' ')
+  end
+
+end
