9fd56b3aa57c4dd6645917f81588e0c17e6efaa1
Alireza Andersen <alireza.andersen@gmail.com>
2018-07-24 15:02:11 -0600

    solution

--- reverse_sentence_words.rb
+++ reverse_sentence_words.rb
@@ -10,7 +10,7 @@
 class ReverseSentence
 
   def self.reverse_sentence_words(string)
-    string. split(/\s+/).map{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
+    string.split(/\s+/).map{|w|wl=w.length-1;(0..wl).map{|i|w[wl-i]}.join}.join(' ')
   end
 
   def self.reverse_sentence_words(string)
